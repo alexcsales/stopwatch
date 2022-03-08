86b24fb55f56bc50aa71cbcb851dd8824aee6029
Alex Sales <88143360+alexcsales@users.noreply.github.com>
2022-03-08 09:53:16 -0300

    Adding: function of start and logic of buttons.

--- main.js
+++ main.js
@@ -10,3 +10,26 @@ function pegaSegundos(milessimos) {
     timeZone: 'UTC'
   });
 }
+
+function iniciaCronometro() {
+  timer = setInterval(function () {
+    segundos++;
+    relogio.innerHTML = pegaSegundos(segundos);
+  }, 1000);
+}
+
+document.addEventListener('click', function (evento) {
+  const element = evento.target
+
+  if (element.classList.contains('iniciar')) {
+    relogio.classList.remove('zerado');
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaCronometro()
+  }
+  if (element.classList.contains('zerar')) {
+    relogio.classList.add('zerado');
+    clearInterval(timer)
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+  }
